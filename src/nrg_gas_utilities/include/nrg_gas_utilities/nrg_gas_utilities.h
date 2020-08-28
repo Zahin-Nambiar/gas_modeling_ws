ec35a78399c770749d66c0550f79ee8d1a056c49
JackBorer <jackborer@utexas.edu>
2020-08-27 20:02:10 -0500

    Adding measurement decks

--- src/nrg_gas_utilities/include/nrg_gas_utilities/nrg_gas_utilities.h
+++ src/nrg_gas_utilities/include/nrg_gas_utilities/nrg_gas_utilities.h
@@ -1,11 +1,33 @@
 #ifndef NRGGASUTILITIES_H
 #define NRGGASUTILITIES_H
 
+#include <nrg_gas_utilities/GasConcentration.h>
+#include <geometry_msgs/Vector3Stamped.h>
+#include <ros/ros.h>
+#include <ros/console.h>
+
+#include <deque>
 
 namespace nrg_gas
 {
 
+struct WindDeck
+{
+    Vector3Stamped get( ros::Time t );
+    void put( Vector3Stamped vs ); 
 
+private:
+    std::deque<Vector3Stamped> deck_;
+};
+
+struct GasDeck
+{
+    GasConcentration get( ros::Time t );
+    void put( GasConcentration gc ); 
+
+private:
+    std::deque<GasConcentration> deck_;
+};
     
 } // namespace nrg_gas
 
