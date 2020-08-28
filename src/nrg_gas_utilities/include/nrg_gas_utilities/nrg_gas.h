5bc89737135970ce564617111ffdd0cb0644dee7
JackBorer <jackborer@utexas.edu>
2020-08-28 11:55:08 -0500

    Added wind and gas std::decks

--- src/nrg_gas_utilities/include/nrg_gas_utilities/nrg_gas.h
+++ src/nrg_gas_utilities/include/nrg_gas_utilities/nrg_gas.h
@@ -3,15 +3,21 @@
 
 #include <nrg_gas_utilities/GasSource.h>
 #include <nrg_gas_utilities/WindParameters.h>
+#include <nrg_gas_utilities/GasConcentration.h>
+#include <geometry_msgs/Vector3Stamped.h>
 
 #include <ros/ros.h>
 #include <tf2_ros/transform_listener.h>
+
+#include <deque>
 
 namespace nrg_gas
 {
 
 using nrg_gas_utilities::GasSource;
 using nrg_gas_utilities::WindParameters;
+using nrg_gas_utilities::GasConcentration;
+using geometry_msgs::Vector3Stamped;
 
 class NRGGas
 {
@@ -24,6 +30,9 @@ public: //TODO make protected
 
     tf2_ros::Buffer tfBuffer_;
     tf2_ros::TransformListener tfListener; 
+
+    std::deque<Vector3Stamped> wind_measurements_;
+    std::deque<GasConcentration> gas_measurements_;
 };
     
 } // namespace nrg_gas
