f65482dff7182a848bfecc3857057e1deb832e88
JackBorer <jackborer@utexas.edu>
2020-09-02 16:17:26 -0500

    Built out initialization function

--- src/nrg_gas_source_localization/src/nrg_gas_source_localization/nrg_gas_source_localization.cpp
+++ src/nrg_gas_source_localization/src/nrg_gas_source_localization/nrg_gas_source_localization.cpp
@@ -1,4 +1,6 @@
 #include <nrg_gas_source_localization/nrg_gas_source_localization.h>
+#include <nrg_gas_utilities/nrg_gas_utilities.h>
+
 
 
 namespace nrg_gas
@@ -17,6 +19,25 @@ NRGGasSourceLocalization::NRGGasSourceLocalization()
     private_nh_.getParam( "state_space_bounds_rate", state_space_[3]); 
 
     initialize(np);
+}
+
+void NRGGasSourceLocalization::initialize(int& np)
+{
+    particle_set_.resize(np);
+    for(auto& particle: particle_set_)
+    {
+        particle.weight = 1.0/np;
+        auto rnv = uniform_rn(4);
+        particle.source.position.point.x = state_space_[0][0] + rnv[0]*(state_space_[0][1] - state_space_[0][0]);
+        particle.source.position.point.y = state_space_[1][0] + rnv[1]*(state_space_[1][1] - state_space_[1][0]);
+        particle.source.position.point.z = state_space_[2][0] + rnv[2]*(state_space_[2][1] - state_space_[2][0]);
+        particle.source.rate = state_space_[3][0] + rnv[3]*(state_space_[3][1] - state_space_[3][0]);
+    }
+}
+
+void NRGGasSourceLocalization::reweight()
+{
+    
 }
 
 } // namespace nrg_gas
