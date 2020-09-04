5575c099895e12e2d94e930fbf8ed2ef4264f153
JackBorer <jackborer@utexas.edu>
2020-09-04 11:11:09 -0500

    First commit

---
+++ src/nrg_gas_concentration/include/nrg_gas_concentration/nrg_gas_concentration.h
@@ -0,0 +1,34 @@
+#ifndef NRG_GAS_CONCENTRATION_H
+#define NRG_GAS_CONCENTRATION_H
+
+#include <nrg_gas_utilities/nrg_gas.h>
+
+#include <nrg_gas_concentration/SetSource.h>
+
+
+namespace nrg_gas
+{
+
+class NRGGasConcentration: protected NRGGas
+{
+public:
+    bool addSource( SetSource::Request& req, SetSource::Response& res );
+
+    // bool getConcentration( GetConcentration::Request &req, GetConcentration::Response &res );
+
+    // bool clearSources( std_srvs::Empty::Request &req, std_srvs::Empty::Response &res );
+
+    std::vector<nrg_gas_utilities::GasSource> sources_;
+
+    ros::ServiceServer set_gas_source_srv_, 
+    //                    get_concentration_srv_, 
+    //                    clear_sources_srv_ ;
+
+private:
+};
+
+
+} // namespace nrg_gas
+
+
+#endif
