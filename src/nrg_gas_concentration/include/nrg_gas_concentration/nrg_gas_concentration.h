8082cdc0bf0c1c61869d587b25e3b8cb4257ae6a
JackBorer <jackborer@utexas.edu>
2020-09-06 14:09:09 -0500

    Added all sers and visualization topic

--- src/nrg_gas_concentration/include/nrg_gas_concentration/nrg_gas_concentration.h
+++ src/nrg_gas_concentration/include/nrg_gas_concentration/nrg_gas_concentration.h
@@ -2,9 +2,14 @@
 #define NRG_GAS_CONCENTRATION_H
 
 #include <nrg_gas_utilities/nrg_gas.h>
-
 #include <nrg_gas_concentration/SetSource.h>
+#include <nrg_gas_concentration/GetConcentration.h>
 
+#include <visualization_msgs/Marker.h>
+
+#include <std_srvs/Empty.h>
+
+//#include <ros/ros.h>
 
 namespace nrg_gas
 {
@@ -12,17 +17,24 @@ namespace nrg_gas
 class NRGGasConcentration: protected NRGGas
 {
 public:
-
+    NRGGasConcentration();
 private:
-    bool addSource( SetSource::Request& req, SetSource::Response& res );
+    bool addSource( nrg_gas_concentration::SetSource::Request& req, nrg_gas_concentration::SetSource::Response& res );
+    bool clearSources( std_srvs::Empty::Request &req, std_srvs::Empty::Response &res );
+    bool getGasConcentration( nrg_gas_concentration::GetConcentration::Request &req, nrg_gas_concentration::GetConcentration::Response &res );
 
-    // bool getConcentration( GetConcentration::Request &req, GetConcentration::Response &res );
+    ros::NodeHandle nh_;
 
-    // bool clearSources( std_srvs::Empty::Request &req, std_srvs::Empty::Response &res );
+    ros::Publisher visualization_pub_;
+
+    ros::ServiceServer set_gas_source_srv_, 
+                       clear_gas_source_srv_,
+                       get_gas_concentration_srv_;
 
     std::vector<nrg_gas_utilities::GasSource> sources_;
 
-    ros::ServiceServer set_gas_source_srv_;
+    visualization_msgs::Marker createSourceMarker(const nrg_gas_utilities::GasSource& source);
+    
 };
 
 
